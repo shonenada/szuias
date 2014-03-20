b911e79e7cdca60c3bcca26481088d578e04572f
shonenada <shonenada@gmail.com>
2014-03-21 00:14:12 +0800

    update

--- szuias/controllers/master.php
+++ szuias/controllers/master.php
@@ -6,20 +6,22 @@ use \Model\Article;
 
 return array(
     "export" => function($app) {
+
         $app->get("/", function() use($app) {
             $home = true;
-            $mid = 2;
-            $slider = array(
-                'nums' => Setting::get('index_slider', 'nums'),
-
-            );
             $news = Article::get_list_by_top_menu(10, 6, 'sort', false);
+            $undergraduate = Article::get_list_by_top_menu(6, 10, 'sort', false);
+            $graduate = Article::get_list_by_top_menu(6, 11, 'sort', false);
+            $research = Article::get_list_by_top_menu(6, 12, 'sort', false);
+            $working = Article::get_list_by_top_menu(7, 17, 'sort', false);
             $app->render("index.html", get_defined_vars());
         });
+
         $app->get('/captcha', function () {
             $captcha = new Captcha(90, 30, 4);
             $captcha->showImg();
             $_SESSION['captchaCode'] = strtolower($captcha->getCaptcha());
         });
+
     }
 );
