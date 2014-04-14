473b45ce4405ae092014cfe4f0c3901fb9c7b884
shonenada <shonenada@gmail.com>
2014-04-14 12:52:10 +0800

    update config of view

--- szuias/config/view.php
+++ szuias/config/view.php
@@ -7,9 +7,13 @@
 return array(
     // 模版引擎配置
     'config' => array(
-        // 开启调试模式，生产环境中请关闭调试模式。
-        'debug' => true,
-        // 缓存的目录
-        'cache' => APPROOT. '/cache',
+        'environment' => array(
+            // 开启调试模式，生产环境中请关闭调试模式。
+            'debug' => true,
+            // 缓存的目录
+            'cache' => APPROOT. 'cache',
+            'auto_reload' => true,      // 检测模版修改自动重编译
+            'optimizations' => -1,      // 优化系数
+        ),
     )
 );
