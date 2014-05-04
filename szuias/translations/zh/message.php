eb712a0a08e6e1cbddafee8559a5b49e9f2ce700
shonenada <shonenada@gmail.com>
2014-05-04 13:36:09 +0800

    update translation

--- szuias/translations/zh/message.php
+++ szuias/translations/zh/message.php
@@ -4,6 +4,12 @@ return array(
     'Institute for Advanced Study in Shenzhen Univeristy' => '深圳大学 高等研究院',
     'Introduce to Institute' => '学院介绍',
     'Institution' => '组织机构',
+    'News' => '新闻动态',
+    'EDU for Undergraduate' => '本科教育',
+    'EDU for Graduate' => '研究生教育',
+    'Scientific Research' => '科研信息',
+    'Teachers' => '教师风采',
+    'Hiring' => '求职招聘',
     'Contact' => '联系我们',
     'Backend' => '进入后台',
 );
