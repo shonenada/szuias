f06b26c18fda6ba4d9f7d8512e51fd64621b20ea
shonenada <shonenada@gmail.com>
2014-05-15 23:05:22 +0800

    fixed bug

--- szuias/Controller/Article.php
+++ szuias/Controller/Article.php
@@ -2,15 +2,13 @@
 
 namespace Controller;
 
-use \Model\Article;
-
 class Article extends Base {
 
     static public $url = '/article/:id';
     static public $conditions = array('id' => '\d+');
 
     static public function get($id) {
-        $article = Article::find($id);
+        $article = \Model\Article::find($id);
         if ($article == null || $article->is_hide || $article->is_deleted) {
             return self::redirect('/');
         }
