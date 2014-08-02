b9e9e873295eb0869312ddb3de6910f421602848
shonenada <shonenada@gmail.com>
2014-08-02 15:02:31 +0800

    do not use jquery..wtf 360 and ie 6

--- szuias/Controller/ClassApply.php
+++ szuias/Controller/ClassApply.php
@@ -9,10 +9,11 @@ class ClassApply extends Base {
     static public $url = '/apply';
 
     static public function get() {
+        $identity_error = false;
         $show_identity = true;
         $identity = self::$request->get('identity');
         $application = ClassApplication::findOneBy(array('identity' => $identity));
-        if ($application != NULL || is_numeric($identity)) {
+        if ($application != NULL || (is_numeric(substr($identity, 0, 16)) && strlen($identity) == 18)) {
             $show_identity = false;
         }
         return self::render("class_application.html", get_defined_vars());
@@ -29,52 +30,55 @@ class ClassApply extends Base {
         }
 
         $not_empty = array(
-            array('name', '姓名'),
-            array('gender', '性别'),
-            array('birthday', '出生日期'),
-            array('identity', '身份证号'),
-            array('middle_school', '毕业中学'),
-            array('exam_id', '准考证号'),
-            array('phone', '手机号码'),
-            array('email', 'Email'),
-            array('intro', '个人简介'),
-            array('reason', '报考理由'),
-            array('total', '高考总分'),
-            array('chinese', '语文'),
-            array('math', '数学'),
-            array('english', '英语'),
-            array('science', '理综'),
-            array('source', '生源地'),
-            array('score_line', '生源地一本分数线'),
-            array('parent', '家长姓名'),
-            array('relationship', '关系'),
-            array('parent_phone', '联系电话'),
-            array('parent_addr', '通信地址'),
+            'name',
+            'gender',
+            'birthday',
+            'identity',
+            'middle_school',
+            'exam_id',
+            'phone',
+            'email',
+            'intro',
+            'reason',
+            'total',
+            'chinese',
+            'math',
+            'english',
+            'science',
+            'source',
+            'score_line',
+            'parent',
+            'relationship',
+            'parent_phone',
+            'parent_addr',
         );
 
+        $not_empty_errors = array();
+
         foreach($not_empty as $field) {
-            if (!isset($post[$field[0]]) || empty($post[$field[0]])) {
+            if (!isset($post[$field]) || empty($post[$field])) {
                 $success = false;
-                $tips = $field[1] . '不能为空';
+                $not_empty_errors[] = $field;
             }
         }
 
         if ($success) {
             $numeric = array(
-                array('identity', '身份证号'),
-                array('phone', '手机号码'),
-                array('total', '高考总分'),
-                array('chinese', '语文成绩'),
-                array('math', '数学成绩'),
-                array('english', '英语成绩'),
-                array('science', '理综成绩'),
-                array('parent_phone', '家长联系电话'),
+                'phone',
+                'total',
+                'chinese',
+                'math',
+                'english',
+                'science',
+                'parent_phone',
             );
 
+            $numeric_errors = array();
+
             foreach($numeric as $field) {
-                if (!is_numeric($post[$field[0]])) {
+                if (!is_numeric($post[$field])) {
                     $success = false;
-                    $tips = $field[1] . '必须为数字';
+                    $numeric_errors[] = $field;
                 }
             }
         }
@@ -115,7 +119,12 @@ class ClassApply extends Base {
             $apply->parent_addr = $post['parent_addr'];
             $apply->save();
         }
-
-        return json_encode(array('success' => $success, 'info' => $tips));
+        if (!$success) {
+            return self::render("class_application.html", get_defined_vars());
+        }
+        else {
+            return '<html><head><script>alert("提交成功！");location.href="/";</script></head></html>';
+        }
+        // return json_encode(array('success' => $success, 'info' => $tips));
     }
 }
