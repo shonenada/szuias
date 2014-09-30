75f0d4796f794be2e224d76f94c76bb4dc753f7f
shonenada <shonenada@gmail.com>
2014-09-30 09:24:57 +0800

    udpate

--- szuias/Controller/MenuList.php
+++ szuias/Controller/MenuList.php
@@ -25,10 +25,11 @@ class MenuList extends Base {
         $menu = Menu::find($mid);
         $page = self::$request->get('page') ? self::$request->get('page') : 1;
         $pagesize = self::$app->config('pagesize');
-        if (in_array($mid, array(14, 15))
+        if (in_array($mid, array(14, 15))) {
             $sort_order = 'ASC';
-        else
+        } else {
             $sort_order = 'DESC';
+        }
         $articles = Article::getListByMenuId($page, $pagesize, $mid, array(array('is_top', 'DESC'), array('sort', $sort_order), array('created', 'DESC')));
         $countOfArticles = Article::countByMids(array($mid));
         $totalPage = ceil($countOfArticles[1] / $pagesize);
--- szuias/Extension/Auth.php
+++ szuias/Extension/Auth.php
@@ -26,7 +26,7 @@ class Auth {
             $auth = new \RBAC\Authentication();
             $auth->load($ptable);
             if (!$auth->accessiable($user, $resource, $method)) {
-                $app->halt(403, "Cookies is expired, please sign in again.");
+                $app->redirect('/account/signin');
             }
         });
     }
